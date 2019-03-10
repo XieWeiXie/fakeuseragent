332b58fe24114513b2232fe13a4da7c94fa71d7b
XieWei <wuxiaoshen@shu.edu.cn>
2019-03-10 15:42:16 +0800

    docs: package docs

---
+++ doc.go
@@ -0,0 +1,21 @@
+// Package fakeuseragent provide fake user agent.
+//
+// Example:
+//
+//	func main() {
+//		fakeUserAgent := application.NewFakeUserAgent(false)
+//		fmt.Println(fakeUserAgent.Random())
+//		fmt.Println(fakeUserAgent.Safari())
+//		fmt.Println(fakeUserAgent.Chrome())
+//		fmt.Println(fakeUserAgent.IE())
+//		fmt.Println(fakeUserAgent.Opera())
+//	}
+//
+// Install:
+//
+//	go get github.com/wuxiaoxiaoshen/fakeuseragent/...
+//
+// See Also:
+//
+// Link: https://github.com/wuxiaoxiaoshen/fakeuseragent
+package fakeuseragent
